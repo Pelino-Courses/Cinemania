ba63c3fad5231f683ead6ebf76704d2fdb5208d8
Dusabe Ramazani <ramazidusabe8@gmail.com>
2022-08-15 19:51:54 +0200

    static URLS fixed

--- main_project/urls.py
+++ main_project/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
+from django.urls import path
 from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url, include
+from django.views.static import serve
 
 ...
 
@@ -25,6 +27,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('accounts.urls')),
     path('', include('main_app.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
